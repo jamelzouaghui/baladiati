2d2f43df1682bac492dc85a80a658bbfcc42b149
Jamel <Jamel@DESKTOP-URUS4FT>
2019-02-12 08:21:44 -0800

    reseting password

---
+++ src/Security/SessionIdHandler.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class SessionIdHandler
+{
+
+    protected $session;
+    protected $securityToken;
+    protected $router;
+    protected $maxIdleTime;
+
+    public function __construct($maxIdleTime, SessionInterface $session, TokenStorageInterface $securityToken, RouterInterface $router)
+    {
+        $this->session = $session;
+        $this->securityToken = $securityToken;
+        $this->router = $router;
+        $this->maxIdleTime = $maxIdleTime;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST != $event->getRequestType()) {
+
+            return;
+        }
+
+        if ($this->maxIdleTime > 0) {
+
+            $this->session->start();
+            $lapse = time() - $this->session->getMetadataBag()->getLastUsed();
+
+            if ($lapse > $this->maxIdleTime) {
+                
+                $this->securityToken->setToken(null);
+                $this->session->getFlashBag()->set('info', 'You have been logged out due to inactivity.');
+                  
+               $request = $event->getRequest();
+               $session = $request->getSession();
+               $session->invalidate();
+               $response = new RedirectResponse($this->router->generate('login'));
+               $response->headers->clearCookie('client_id');
+               $response->headers->clearCookie('accesss_token');
+               $response->headers->clearCookie('refresh_token');
+               $response->headers->clearCookie('PHPSESSID');
+               $response->send();
+               $event->setResponse($response);
+            }
+        }
+    }
+}
